06c38f9da74596518cc80a218fa6f5f1fd28ecdf
EunchongKim <slvrcods22@gmail.com>
2024-04-24 21:41:21 +0900

    study update: #3.0Header

--- nomadcoders-flutter-for-beginners/#3 UI CHALLENGE/toonflix/lib/main.dart
+++ nomadcoders-flutter-for-beginners/#3 UI CHALLENGE/toonflix/lib/main.dart
@@ -1,28 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp()); // MyApp() == root widget : App의 시작점
+  runApp(MyApp());
 }
 
-
-class MyApp extends StatelessWidget { 
-  // MyApp을 widget으로 바꿔주기 위해 상속. StatelessWidget은 build 메소드를 필요로 함
-
-  @override // 부모클래스의 build 메소드 구현
+class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    // 어떤 위젯 return 해줄까? 루트 위젯(여기서는 MyApp)은 디자인 시스템을 return해야함.
-    // 디자인 시스템 : material(구글style) 또는 cupertino(IOSstyle)
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          title: Text('Hello Flutter'),
-        ),
-        body: Center(
-          child: Text('hello world'),
-          ),
-        ),
-      );
+          backgroundColor: Color(0xFF181818),
+          body: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Column(children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Hey, Selena',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        Text(
+                          'Welcome back',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.8),
+                            fontSize: 18,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ]))),
+    );
   }
-
 }
