7c5bba7a1a6fd4aa0de1bae6e1c83dede1294b5a
EunchongKim <slvrcods22@gmail.com>
2024-04-25 19:05:36 +0900

    study update: #3.2 Buttons Section

--- nomadcoders-flutter-for-beginners/#3 UI CHALLENGE/toonflix/lib/main.dart
+++ nomadcoders-flutter-for-beginners/#3 UI CHALLENGE/toonflix/lib/main.dart
@@ -12,36 +12,84 @@ class MyApp extends StatelessWidget {
           backgroundColor: Color(0xFF181818),
           body: Padding(
               padding: EdgeInsets.symmetric(horizontal: 40),
-              child: Column(children: [
-                SizedBox(
-                  height: 80,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
+                    SizedBox(
+                      height: 80,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        Text(
-                          'Hey, Selena',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w800,
-                          ),
-                        ),
-                        Text(
-                          'Welcome back',
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.8),
-                            fontSize: 18,
-                          ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Hey, Selena',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                            Text(
+                              'Welcome back',
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(0.8),
+                                fontSize: 18,
+                              ),
+                            )
+                          ],
                         )
                       ],
-                    )
-                  ],
-                ),
-              ]))),
+                    ),
+                    SizedBox(
+                      height: 120,
+                    ),
+                    Text(
+                      'Total Balance',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white.withOpacity(0.8),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      '\$5 194 482',
+                      style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Color(0xFFF2B33A),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(50),
+                              )),
+                          child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                vertical: 20,
+                                horizontal: 50,
+                              ),
+                              child: Text(
+                                'Transfer',
+                                style: TextStyle(
+                                  fontSize: 22,
+                                ),
+                              )),
+                        )
+                      ],
+                    ),
+                  ]))),
     );
   }
 }
