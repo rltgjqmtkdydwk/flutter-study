5381addbe5c4a680ca128947ecf3e562e65c3e6a
EunchongKim <slvrcods22@gmail.com>
2024-05-09 19:35:57 +0900

    study update: #4.4 Widget Lifecycle

--- nomadcoders-flutter-for-beginners/stateful_widgets/lib/main.dart
+++ nomadcoders-flutter-for-beginners/stateful_widgets/lib/main.dart
@@ -12,19 +12,16 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  List<int> numbers = [];
-
-  void onClicked() {
+  bool showTitle = true;
+  void toggleTitle() {
     setState(() {
-      numbers.add(numbers.length);
+      showTitle = !showTitle;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // BuildContext : 위젯 트리에서 위젯의 위치를 제공 -> 먼 부모요소 데이터에 접근할 수 있음!
     return MaterialApp(
-      // theme : app의 스타일시트
       theme: ThemeData(
         textTheme: const TextTheme(
           titleLarge: TextStyle(
@@ -37,8 +34,11 @@ class _AppState extends State<App> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              MyLargeTitle(),
+            children: [
+              showTitle ? const MyLargeTitle() : const Text("nothing"),
+              IconButton(
+                  onPressed: toggleTitle,
+                  icon: const Icon(Icons.remove_red_eye))
             ],
           ),
         ),
@@ -47,20 +47,44 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+// StatefulWidget은 lifecycle이 있다. = 살아있다.
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0; // 일반적인 초기화
+
+  // initState() : 데이터 초기화하는 메서드(특히, 부모요소에 의존하는 데이터 초기화)
+  // build() 이전에 호출. 한번만 호출. 예) api 업데이트 반영.
+  @override
+  void initState() {
+    super.initState();
+    print("init!");
+  }
+
+  // dispose() : 위젯이 스크린에서 제거될 때 호출되는 메서드
+  // 위젯을 위젯 트리에서 제거하기 전에 무언가를 취소. 예) 이벤트 리스너 취소.
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose!");
+  }
+
+  // build() : 위젯에서 UI를 만듦
+  @override
   Widget build(BuildContext context) {
+    print("build!");
     return Text(
       'My Large Title',
       style: TextStyle(
         fontSize: 30,
-        color:
-            Theme.of(context).textTheme.titleLarge?.color, //context를 통해 데이터 참조
-        //(null은 참조할 수 없기 때문에 null safety 필요)
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
