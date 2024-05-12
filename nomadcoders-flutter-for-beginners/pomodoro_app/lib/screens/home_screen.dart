33a8122bdf0d468314ea7e9f321ac04e4e979950
EunchongKim <slvrcods22@gmail.com>
2024-05-12 13:12:08 +0900

    study update: #5.0 User Interface

---
+++ nomadcoders-flutter-for-beginners/pomodoro_app/lib/screens/home_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(children: [
+        Flexible(
+          flex: 1,
+          child: Container(
+            alignment: Alignment.bottomCenter,
+            child: Text(
+              '25:00',
+              style: TextStyle(
+                  color: Theme.of(context)
+                      .cardColor, //BuildContext를 이용해 다른 위젯의 cardColor 참조
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+        ),
+        Flexible(
+            flex: 2,
+            child: Center(
+              child: IconButton(
+                iconSize: 120,
+                color: Theme.of(context)
+                    .cardColor, //BuildContext를 이용해 다른 위젯의 cardColor 참조
+                onPressed: () {},
+                icon: const Icon(Icons.play_circle_outline),
+              ),
+            )),
+        Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context)
+                            .cardColor), //BuildContext를 이용해 다른 위젯의 cardColor 참조
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodors',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge!
+                                  .color),
+                        ),
+                        Text('0',
+                            style: TextStyle(
+                              fontSize: 58,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge! //이미 앞서 정의해줬기 때문에 null이 아니라는 뜻
+                                  .color,
+                            )),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )),
+      ]),
+    );
+  }
+}
