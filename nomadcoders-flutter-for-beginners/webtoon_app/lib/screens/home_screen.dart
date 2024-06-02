1c57abb2227e7a6fa96b1f53e8b80f052e0d8331
EunchongKim <slvrcods22@gmail.com>
2024-06-02 12:27:29 +0900

    study update: #6.5 waitForWebToons

--- nomadcoders-flutter-for-beginners/webtoon_app/lib/screens/home_screen.dart
+++ nomadcoders-flutter-for-beginners/webtoon_app/lib/screens/home_screen.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon_app/model/webtoon_model.dart';
+import 'package:webtoon_app/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+//stateful
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebToons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  //initState() : build메서드 재실행
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons(); //HTTP요청을 기다리는 함수
+  }
 
   @override
   Widget build(BuildContext context) {
--- nomadcoders-flutter-for-beginners/webtoon_app/lib/services/api_service.dart
+++ nomadcoders-flutter-for-beginners/webtoon_app/lib/services/api_service.dart
@@ -4,10 +4,12 @@ import 'package:http/http.dart' as http;
 import 'package:webtoon_app/model/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today"; //API URL의 엔드포인트
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today"; //API URL의 엔드포인트
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  //state가 없는 클래스(static으로)
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = []; //웹툰 리스트
     final url = Uri.parse('$baseUrl/$today'); //url 서버에 요청
     final response = await http.get(url); //서버에 요청한 처리를 기다렸다가 받으면 response로 저장
@@ -16,7 +18,8 @@ class ApiService {
       //jsonDecode(): string을 json으로 디코딩
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
       }
       return webtoonInstances;
     }
