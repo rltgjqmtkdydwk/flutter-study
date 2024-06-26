5841e54dd1d5a2acacd13e93183b215c4fb81f35
EunchongKim <slvrcods22@gmail.com>
2024-06-27 00:39:19 +0900

    study update: #6.8 Webtoon Card

--- nomadcoders-flutter-for-beginners/webtoon_app/lib/screens/home_screen.dart
+++ nomadcoders-flutter-for-beginners/webtoon_app/lib/screens/home_screen.dart
@@ -26,17 +26,14 @@ class HomeScreen extends StatelessWidget {
         builder: (context, snapshot) {
           //snapshot: future의 상태
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.vertical,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                print(index); //메모리 효율을 위해 보여지는 item의 인덱스만 찾는다.
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) => const SizedBox(
-                width: 20,
-              ),
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(child: makeList(snapshot)) //makeList 메소드로 묶어주기
+                //ERROR: makeList는 높이를 알수 없기 때문에 -> Expanded() 사용
+              ],
             );
           } else {
             return const Center(
@@ -44,6 +41,51 @@ class HomeScreen extends StatelessWidget {
             );
           }
         },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.vertical,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(
+          vertical: 10, horizontal: 20), // 이미지 상단 그림자 표시하기 위한 padding
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior:
+                  Clip.hardEdge, //clipBehavior: 자식이 부모영역에 침범하는 것을 제어하는 방법
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black
+                        .withOpacity(0.5), //color를 가장 나중에 설정하면 보기 편함
+                  ),
+                ],
+              ),
+              child: Image.network(webtoon.thumb), //Image 위젯
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
       ),
     );
   }
