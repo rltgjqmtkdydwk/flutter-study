45317e11739c251f4946b03a5ead92968ca03655
EunchongKim <slvrcods22@gmail.com>
2024-06-29 16:40:29 +0900

    fix: simulator image load 403 error

--- nomadcoders-flutter-for-beginners/webtoon_app/lib/main.dart
+++ nomadcoders-flutter-for-beginners/webtoon_app/lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:webtoon_app/screens/home_screen.dart';
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const App());
 }
 
@@ -13,5 +16,15 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: HomeScreen(),
     );
+  }
+}
+
+//user-agent 헤더를 바꿔주어야 함. (http가 유저-클라이언트 통신 방식(=프로토콜)이라서)
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..userAgent =
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
   }
 }
